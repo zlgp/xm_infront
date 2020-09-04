29790f13eab29854c57f44b4c15e49ba1c0f871d
steve <steve@htdtek.com>
2020-09-04 18:37:51 +0800

    增加图标

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+    // 配置图标
+    pwa: {
+        iconPaths: {
+            favicon32: 'favicon.ico',
+            favicon16: 'favicon.ico',
+            appleTouchIcon: 'favicon.ico',
+            maskIcon: 'favicon.ico',
+            msTileImage: 'favicon.ico'
+        }
+    }
+}
